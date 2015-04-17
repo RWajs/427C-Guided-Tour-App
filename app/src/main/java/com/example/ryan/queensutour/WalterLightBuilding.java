8795a5145be01957b9fc1847464cc6fcfd36deb6
RWajs <13rjw15@queensu.ca>
2015-04-17 14:25:03 -0400

    Revert "Potential Picture Fix"
    This reverts commit 6ba3407b995c930fcd1ebbe9ef59199cfc9cddfa.

--- app/src/main/java/com/example/ryan/queensutour/WalterLightBuilding.java
+++ app/src/main/java/com/example/ryan/queensutour/WalterLightBuilding.java
@@ -1,17 +1,11 @@
 package com.example.ryan.queensutour;
 
 import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-
-import java.lang.ref.WeakReference;
 
 
 public class WalterLightBuilding extends ActionBarActivity {
@@ -46,38 +40,5 @@ public class WalterLightBuilding extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void loadBitmap(int resId, ImageView imageView) {
-        BitmapWalterLight task = new BitmapWalterLight(imageView);
-        task.execute(resId);
-    }
-}
-
-class BitmapWalterLight extends AsyncTask<Integer, Void, Bitmap> {
-    private final WeakReference<ImageView> imageViewReference;
-    private int data = 0;
-
-    public BitmapWalterLight(ImageView imageView) {
-        // Use a WeakReference to ensure the ImageView can be garbage collected
-        imageViewReference = new WeakReference<ImageView>(imageView);
-    }
-
-    // Decode image in background.
-    @Override
-    protected Bitmap doInBackground(Integer... params) {
-        data = params[0];
-        return BitmapFactory.decodeFile(String.valueOf(R.drawable.walterlight));
-    }
-
-    // Once complete, see if ImageView is still around and set bitmap.
-    @Override
-    protected void onPostExecute(Bitmap bitmap) {
-        if (imageViewReference != null && bitmap != null) {
-            final ImageView imageView = imageViewReference.get();
-            if (imageView != null) {
-                imageView.setImageBitmap(bitmap);
-            }
-        }
     }
 }
