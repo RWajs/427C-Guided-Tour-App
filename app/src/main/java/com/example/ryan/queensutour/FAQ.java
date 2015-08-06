070a8752aec80ce4a7bcc7fc4e826b5e01bfd362
13mm155 <mohamadhmansour@hotmail.com>
2015-08-06 14:00:22 +0400

    FAQ fixed & layout margins
    The grey buttons need a fix so they dont look too big/small, i'll do it
    later

--- app/src/main/java/com/example/ryan/queensutour/FAQ.java
+++ app/src/main/java/com/example/ryan/queensutour/FAQ.java
@@ -6,6 +6,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FAQ extends ActionBarActivity {
@@ -14,9 +17,202 @@ public class FAQ extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+
+
+        Button q1 = (Button) findViewById(R.id.Question1);
+        q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.Answer1FAQPage);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q2 = (Button) findViewById(R.id.Q2);
+        q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A2);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q3 = (Button) findViewById(R.id.Q3);
+        q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A3);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q4 = (Button) findViewById(R.id.Q4);
+        q4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A4);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q5 = (Button) findViewById(R.id.Q5);
+        q5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A5);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q6 = (Button) findViewById(R.id.Q6);
+        q6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A6);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q7 = (Button) findViewById(R.id.Q7);
+        q7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A7);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q8 = (Button) findViewById(R.id.Q8);
+        q8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A8);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q9 = (Button) findViewById(R.id.Q9);
+        q9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A9);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q10 = (Button) findViewById(R.id.Q10);
+        q10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A10);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q11 = (Button) findViewById(R.id.Q11);
+        q11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A11);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q12 = (Button) findViewById(R.id.Q12);
+        q12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A12);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q13 = (Button) findViewById(R.id.Q13);
+        q13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A13);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q14 = (Button) findViewById(R.id.Q14);
+        q14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A14);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+        Button q15 = (Button) findViewById(R.id.Q15);
+        q15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView a = (TextView) FAQ.this.findViewById(R.id.A15);
+                if (a.getVisibility() == View.GONE)
+                    a.setVisibility(View.VISIBLE);
+                else if (a.getVisibility() == View.VISIBLE)
+                    a.setVisibility(View.GONE);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
